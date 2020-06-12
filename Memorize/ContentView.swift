1de82dda15bcc9a8af1ccc9a4f5777e653549d6c
Maksim Kim <maksim.kim@bellerage.ru>
2020-06-12 13:59:50 +0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Maksim Kim on 12.06.2020.
+//  Copyright © 2020 Bellerage. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
